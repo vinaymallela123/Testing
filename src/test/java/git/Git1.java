3c50b8d5e4ed7e020ddce210268a5cfcecdfffd1
Vinay Mallela <vinayreddymallela96529@gmail.com>
2024-08-21 10:34:43 +0530

    Vinay

--- src/test/java/git/Git1.java
+++ src/test/java/git/Git1.java
@@ -3,10 +3,28 @@ package git;
 public class Git1 {
 
 	public static void main(String[] args) {
-		System.out.print("Vinay ");
+		/*System.out.print("Vinay ");
 		System.out.print("Kumar Reddy");
 		System.out.println("IQE");
-		System.out.println("4th Commit");
+		System.out.println("4th Commit");*/
+		
+		int num=10;
+		long num1=num;
+
+		float avg=10.5f;
+		double avg1=avg;
+		
+		System.out.println(num1);
+		System.out.println(avg1);
+		
+		long number=9876456;
+		int number1=(int) number;
+		System.out.println(number1);
+		
+		double average=45.555;
+		float average1=(float) average;
+		System.out.println(average1	);
+		
 
 	}
 
